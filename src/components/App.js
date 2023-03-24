a6b164c18e3586d1d0e9de1f124cc2386a62fb0e
Shankar-pathade <109196454+Shankar-pathade@users.noreply.github.com>
2023-03-24 14:31:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,35 +2,48 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 
 const App = () => {
-  const [rating, setRating] = useState();
-  const [comment, setComment] = useState();
-  const [commentError, setCommentError] = useState();
+  const [rating, setRating] = useState(5);
+  const [comment, setComment] = useState('');
+  const [commentError, setCommentError] = useState('');
+
+  const handleRatingChange = (event) => {
+    setRating(event.target.value);
+  }
+
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+    if (event.target.value.length < 5) {
+      setCommentError('Comment must be atleast 5 characters');
+    } else {
+      setCommentError('');
+    }
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+  }
 
   return (
-    <div id="main">
-      <form>
-        <div>
-          <label htmlFor='rating'>Rating: </label>
-          <input 
-            type="range"
-            min="1"
-            max="10"
-            id="rating"
-          />
-          <span className='rating'>rating</span>
-        </div>
-        <div>
-          <label htmlFor='comment'>Comment: </label>
-          <textarea 
-            id='comment'
-          />
-          <p style={{ color: 'red' }} className="comment-error">Comment must be atleast 5 characters.</p>
-        </div>
-        <button type='submit'>Submit</button>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Rating:
+          <input type="range" min="1" max="10" value={rating} onChange={handleRatingChange} />
+          <span className="rating">{rating}</span>
+        </label>
+        <br />
+        <label>
+          Comment:
+          <textarea value={comment} onChange={handleCommentChange} />
+          <p className="comment-error">{commentError}</p>
+        </label>
+        <br />
+        <button type="submit">Submit</button>
       </form>
     </div>
-  )
+  );
 }
 
-
 export default App;
+
